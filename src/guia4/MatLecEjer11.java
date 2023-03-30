12dece69b89288d53143f3c03e54fd6b4bb95b3e
Ariel <arielo14@gmail.com>
2023-03-30 16:10:23 -0300

    ejercicio 11 de guia

---
+++ src/guia4/MatLecEjer11.java
@@ -0,0 +1,89 @@
+/*
+ Escribir un programa que procese una secuencia de caracteres ingresada por teclado 
+y terminada en punto, y luego codifique la palabra o frase ingresada de la siguiente 
+manera: cada vocal se reemplaza por el carácter que se indica en la tabla y el resto 
+de los caracteres (incluyendo a las vocales acentuadas) se mantienen sin cambios.
+a - @
+e - #
+i - $
+o - %
+u - *
+
+Realice un subprograma que reciba una secuencia de caracteres y retorne la codificación 
+correspondiente. Utilice la estructura “según” para la transformación. 
+Por ejemplo, si el usuario ingresa:     Ayer, lunes, salimos a las once y 10.
+La salida del programa debería ser:  @y#r, l*n#s, s@l$m%s @ l@s %nc# y 10.
+
+ */
+package guia4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cmartinez
+ */
+public class MatLecEjer11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int tam =0;
+       
+       Scanner leer = new Scanner(System.in);
+       System.out.println("Por favor ingrese una frase: ");
+       String palabra = leer.nextLine();
+
+       System.out.println("la frase codificada es: "+codificacion(palabra) );
+       
+       
+       
+       
+//       
+//        tam = texto.length();
+//        
+//        System.out.println("El texto ingresado es " + texto);
+//        System.out.println("El tamaño es "+tam);
+//        System.out.println("la primer letra es "+ texto.charAt(0));
+        
+
+    }
+    public static String codificacion (String texto){
+        String frase="";
+        int tam=0;
+        
+        tam = texto.length();
+        System.out.println("el tamaño es: " + tam);
+        
+        for (int i=0; i<tam; i++){
+            char letra = texto.charAt(i);
+            
+            switch (texto.toLowerCase(texto.charAt(i))){
+                case 'a':
+                    frase= frase+"@";
+                    break;
+                case 'e':
+                    frase= frase+"#";
+                    break;
+                case 'i':
+                    frase= frase+"$";
+                    break;
+                case 'o':
+                    frase= frase+"%";
+                    break;
+                case 'u':
+                    frase= frase+"*";
+                    break;
+                default:
+                    frase= frase+String.valueOf(texto.charAt(i));
+            }
+                    
+        }
+        
+        
+        return frase;
+    }
+            
+}
